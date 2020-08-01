f471a0d15aa36817c9c7cd70f59f0b9f0d0107b3
jduan22 <55414658+jduan22@users.noreply.github.com>
2020-08-01 10:45:34 -0700

    Update script.js
    Fixed bugs in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var slider = document.getElementById("myRange");//This code has a few mistakes. myRange is a class not an Id and demo is a type, not an Id.
-var output = document.getElementById("demo");
+var slider = document.getElementById(id="myRange");//This code has a few mistakes. myRange is a class not an Id and demo is a type, not an Id.
+var output = document.getElementByClass("demo");
 output.innerHTML = slider.Value;
 
-slider.oninput = function() { 
+slider.oninput = function() {
     output.innerHTML = this.value;
 }
